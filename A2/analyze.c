0895125fe5ccb67f252cdd177b5eaef561a3bef0
Rafael Solorzano <rafa@solorzanoc.com>
2016-06-23 11:57:59 -0700

    Flags counter

--- A2/analyze.c
+++ A2/analyze.c
@@ -67,7 +67,12 @@ void print_connections(){
   for (; i < total_connections; i++){
     printf("Connection %d:\nSource Address: %s\nDestination Address: %s\nSource Port: %d\n", i+1, connections[i].ip_src,connections[i].ip_dst, connections[i].port_src);
     printf("Destinantion Port: %d\n", connections[i].port_dst);
-    printf("Status:\n");
+    if (connections[i].rst_count > 1){
+    printf("Status: R\n");
+    }
+    else {
+      printf("Status: \n");
+    }
     //if complete printf
     printf("Start Time:\n");
     printf("EndTime:\n");
@@ -180,6 +185,7 @@ void check_connection(struct ip *ip, struct TCP_hdr *tcp, struct timeval ts, con
     connections[total_connections].port_dst = ntohs(tcp->th_dport);
     connections[total_connections].is_set = 1;
     connections[total_connections].starting_time = ts;
+    //printf("flag is %d\n",tcp->th_flags);
     if (tcp->th_flags == TH_FIN){
       connections[total_connections].fin_count+=1;
     } else if (tcp->th_flags == TH_SYN){
@@ -216,6 +222,16 @@ void check_connection(struct ip *ip, struct TCP_hdr *tcp, struct timeval ts, con
     connections[total_connections].syn_count+=1;
   } else if (tcp->th_flags == TH_RST){
     connections[total_connections].rst_count+=1;
+  }
+} else {
+  //match is at i
+  //we have a match and have to handle modify the connection to which packet matched
+  if (tcp->th_flags == TH_FIN){
+    connections[i].fin_count+=1;
+  } else if (tcp->th_flags == TH_SYN){
+    connections[i].syn_count+=1;
+  } else if (tcp->th_flags == TH_RST){
+    connections[i].rst_count+=1;
   }
 }
 
